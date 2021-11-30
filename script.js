586d96934b8a7828fc70633d5837a25ccf9e5c53
InRectoVirtus <inrectovirtus.8.32@gmail.com>
2021-11-30 17:38:17 +0200

    Fix: new

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-const global = document.getElementById('global')
-const tdInput = document.getElementById('first')
-const tdList = document.getElementById('third')
+const taskTextInput = document.getElementById('taskText');
+const list = document.getElementById('list');
+const addTaskBtn = document.getElementById('addTaskBtn');
 
-global.addEventListener('click', (a) => {
-    let target = a.target
-    if (target.type === 'submit') {
-        a.preventDefault()
-        if(tdInput.value.length === 0) {
-            tdInput.value = 'Empty'
-        } else {
-            creator(tdInput, tdList)
-        }
+addTaskBtn.addEventListener('click', (e) => {
+    if (!!taskTextInput.value) {
+        addListItem(taskTextInput.value);
+        taskTextInput.value = '';
+    } else {
+        alert('Enter task name');
     }
 })
 
-function creator(i, l) {
-    let block = document.createElement('div');
-    block.classList = 'block';
-    let elem = document.createElement('li');
-    elem.innerText = i.value;
-    elem.classList = 'block_elem';
-    block.append(elem);
-    let yes = document.createElement('button');
-    yes.innerHTML = 'Сделано';
-    yes.classList = 'yes';
-    block.append(yes);
-    let delet = document.createElement('button');
-    delet.innerHTML = `Удалить ${i.value}`;
-    delet.classList = 'delet';
-    block.append(delet);
-    l.append(block);
-    i.value = '';
+const addListItem = (value) => {
+    let listItem = document.createElement('li');
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'Delete Task';
+    deleteBtn.addEventListener('click', () => {
+        deleteBtn.parentElement.remove();
+    });
+
+    listItem.innerText = `${value}`;
+    listItem.classList.add('list-item');
+    listItem.classList.add('list-item__todo');
+    listItem.append(deleteBtn);
+
+    list.append(listItem);
 }
 
-tdList.addEventListener('click', function (a) {
-    let c = a.target
-    if(c.classList[0] === 'yes') {
-        toggler(c)
-    } else if (c.classList[0] === 'delet') {
-        deleteBlock(c)
-        //[0] key-обращ
+list.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target !== list) {
+        toggleColor(target);
     }
 })
-const toggler = (c) => {
-    const parental = c.parentElement
-    if(parental) {
-        parental.classList.toggle('green')
-    } else if(parental.classList = 'green') {
-        parental.classList.toggle('green')
-    } 
-}
-const deleteBlock = (c) => {
-    const parental = c.parentElement
-    parental.classList = 'null'
-    parental.remove()
-}
+
+const toggleColor = (elem) => elem.classList.toggle('list-item__todo');
